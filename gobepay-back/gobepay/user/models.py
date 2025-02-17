89899449847f61cc47ed585687e5d60706959c2f
Alangobe <alangobe01@gmail.com>
2025-02-17 18:04:53 -0500

    modificada vista para crear clientes en stripe al registrar uno nuevo - corregido

--- gobepay-back/gobepay/user/models.py
+++ gobepay-back/gobepay/user/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class User(AbstractUser):
+    
     email = models.EmailField(_('email address'), unique=True)
     username = models.CharField(max_length=150, unique=False, blank=True)
     id_user_stripe = models.CharField(max_length=255, blank=True)
--- gobepay-back/gobepay/user/views.py
+++ gobepay-back/gobepay/user/views.py
@@ -2,6 +2,7 @@ import stripe
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from user.models import User
 from user.serializers import UserRegisterSerializer
 
 # Create your views here.
@@ -10,9 +11,18 @@ class RegisterView(APIView):
     
     def post(self, request):
         
-        serializer = UserRegisterSerializer(data=request.data)
+        serializer = UserRegisterSerializer(data=request.data)             # Envío el cuerpo de request para el serializador  
+        
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
+            
+            new_user = serializer.save()
+            email_user = serializer.data['email']                          # Se crea el id de stripe a partir del email registrado
+            user_stripe = stripe.Customer.create(email=email_user)         # Crea un nuevo usuario para stripe
+            id_user_stripe = user_stripe.id                                # Extraigo el id del nuevo usuario creado de stripe
+            user = User.objects.get(pk=new_user.id)                        # Extraigo el id del usuario que es su pk
+            user.id_user_stripe = id_user_stripe                           # El id creado en stripe se asigna al id en la BD
+            user.save()                                                    # Se guarda el nuevo user
+            
             return Response(serializer.data, status=status.HTTP_200_OK)
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
