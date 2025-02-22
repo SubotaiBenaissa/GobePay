8b45a84a5d819595f2aa3ebac7af50ba1ebd99ef
Alangobe <alangobe01@gmail.com>
2025-02-21 23:28:44 -0500

    creado modelo de wallet

--- gobepay-back/gobepay/wallet/models.py
+++ gobepay-back/gobepay/wallet/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Wallet(models.Model):
+    
+    balance = models.DecimalField(max_digits=100, decimal_places=2)
+    
+    def __str__(self):
+        
+        return str(self.id)
