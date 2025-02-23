d0cffccc87e2d2d5e26fd315c005c9b5635a1519
Alangobe <alangobe01@gmail.com>
2025-02-22 23:06:46 -0500

    iniciada configuración de jwt para login de usuario

--- gobepay-back/gobepay/gobepay/settings.py
+++ gobepay-back/gobepay/gobepay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,6 +103,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=90)
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
--- gobepay-back/gobepay/user/router.py
+++ gobepay-back/gobepay/user/router.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 from user.views import RegisterView
 
 urlpatterns = [
     path("auth/register", RegisterView.as_view()),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain')
 ]
