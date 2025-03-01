c9d95867412c59a20890abed9a3a81a49aa30d14
Alangobe <alangobe01@gmail.com>
2025-02-28 23:18:56 -0500

    creada vista para enviar dinero

--- gobepay-back/gobepay/wallet/views.py
+++ gobepay-back/gobepay/wallet/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from wallet.models import Wallet
+from user.models import User
 
 # Create your views here.
 load_dotenv()
@@ -17,7 +18,10 @@ class RechargeView(APIView):
     
     def post(self, request):
         
-        totalAmout = int(float(request.data['amount'])*100)
+        totalAmout = int(float(request.data['amount'])*100)   # Se multiplica por 100 por los decimales
+        # print(request.data['token'])
+        # print(request.data['amount'])
+        # print(totalAmout)
         stripe.Charge.create(
             source = request.data['token'],
             amount = totalAmout,
@@ -26,5 +30,23 @@ class RechargeView(APIView):
     
         wallet = Wallet.objects.get(pk=request.user.wallet_id)
         wallet.balance = float(wallet.balance) + float(request.data['amount'])
+        wallet.save()
+        
+        return Response('OK', status=status.HTTP_200_OK)
+    
+class SendMoneyView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        
+        wallet = Wallet.objects.get(pk=request.user.wallet_id)
+        wallet.balance = float(wallet.balance) - float(request.data['amount'])
+        wallet.save()
+        
+        user_dest = User.objects.get(pk=request.data['userId'])
+        wallet_dest = Wallet.objects.get(pk=user_dest.wallet_id)
+        wallet_dest = float(wallet_dest.balance) + float(request.data['amount'])
+        wallet_dest.save()
         
         return Response('OK', status=status.HTTP_200_OK)
