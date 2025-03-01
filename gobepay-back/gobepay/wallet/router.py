ac676ae6d01a2e253f0e1b3feec4fdd3ec4986d9
Alangobe <alangobe01@gmail.com>
2025-02-28 23:20:38 -0500

    vista en sendmoney en router de wallet

--- gobepay-back/gobepay/wallet/router.py
+++ gobepay-back/gobepay/wallet/router.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from wallet.views import RechargeView
+from wallet.views import RechargeView, SendMoneyView
 
 urlpatterns = [
-    path('wallet/recharge/', RechargeView.as_view())
+    path('wallet/recharge/', RechargeView.as_view()),
+    path('wallet/send/', SendMoneyView.as_view())
 ]
