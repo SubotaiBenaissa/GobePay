173e817e9b93c57a8942598161bd337b5b79fe90
Alangobe <alangobe01@gmail.com>
2025-03-01 22:53:37 -0500

    registrado history en admin

--- gobepay-back/gobepay/history/admin.py
+++ gobepay-back/gobepay/history/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from history.models import History
 
 # Register your models here.
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+    
+    list_display = ['type', 'origin_user', 'target_user', 'amount', 'created_at']
+    fieldsets = (
+        ('Información básica', {
+            'fields': ['type', 'amount', 'origin_user' 'target_user', 'message', 'created_at']
+        }),
+    )
