f31006ee503aa7e976f9a1f31e60fc14263aac1c
Alangobe <alangobe01@gmail.com>
2025-03-02 20:33:12 -0500

    asignado historial a usuarios

--- gobepay-back/gobepay/wallet/views.py
+++ gobepay-back/gobepay/wallet/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from wallet.models import Wallet
 from user.models import User
+from history.models import History
 
 # Create your views here.
 load_dotenv()
@@ -19,18 +20,27 @@ class RechargeView(APIView):
     def post(self, request):
         
         totalAmout = int(float(request.data['amount'])*100)   # Se multiplica por 100 por los decimales
-        # print(request.data['token'])
-        # print(request.data['amount'])
-        # print(totalAmout)
-        stripe.Charge.create(
-            source = request.data['token'],
-            amount = totalAmout,
-            currency = "usd"
-        )
+        print(request.data['token'])
+        print(request.data['amount'])
+        print(totalAmout)
+        # stripe.Charge.create(
+        #     source = request.data['token'],
+        #     amount = totalAmout,
+        #     currency = "usd"
+        # )
     
         wallet = Wallet.objects.get(pk=request.user.wallet_id)
         wallet.balance = float(wallet.balance) + float(request.data['amount'])
         wallet.save()
+        
+        history = History(
+            type="recharge",
+            origin_user=self.request.user,
+            target_user=self.request.user,
+            amount=request.data['amount']
+        )
+        
+        history.save()
         
         return Response('OK', status=status.HTTP_200_OK)
     
