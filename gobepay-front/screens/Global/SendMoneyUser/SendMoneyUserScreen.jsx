1e13f873e78ed5774b99e4eb03ff5f6f9e87e955
Alangobe <alangobe01@gmail.com>
2025-03-22 20:15:15 -0500

    creadas más pantallas xd

---
+++ gobepay-front/screens/Global/SendMoneyUser/SendMoneyUserScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { styles } from './SendMoneyUserScreen.styles'
+
+export default function SendMoneyUserScreen() {
+
+    return (
+        <View>
+            <Text>SendMoneyUserScreen</Text>
+        </View>
+    )
+
+}
