eb72808cf1abeb31643a540b9d42b2cbb768a9eb
Alangobe <alangobe01@gmail.com>
2025-03-22 19:05:17 -0500

    iniciados componentes de screens

---
+++ gobepay-front/screens/Account/ChangeEmailScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ChangeEmailScreen() {
+
+
+    return (
+        <View>
+            <Text>ChangeEmailScreen</Text>
+        </View>
+    )
+    
+}
