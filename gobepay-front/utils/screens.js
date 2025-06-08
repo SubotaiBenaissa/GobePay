712199daf087edaa99915b31e1f5a64b3ac19151
Alangobe <alangobe01@gmail.com>
2025-06-08 16:21:13 -0500

    definidos nombres de screens

---
+++ gobepay-front/utils/screens.js
@@ -0,0 +1,47 @@
+export const auth = {
+    root: "AuthRoot",
+    loginScreen: "LoginScreen",
+    registerScreen: "RegisterScreen"
+}
+
+export const home = {
+    root: "HomeRoot",
+    homeScreen: "HomeScreen"
+}
+
+export const payments = {
+    root: "PaymentsRoot",
+    paymentScreen: "PaymentScreen"
+}
+
+const wallet = {
+    root: "WalletRoot",
+    walletScreen: "WalletScreen",
+    amountRechargeScreen: "AmountRechargeScreen",
+    rechargeScreen: "RechargeScreen"
+}
+
+const account = {
+    root: "AccountRoot",
+    accountScreen: "AccountScreen",
+    changeFullNameScreen: "ChangeFullNameScreen",
+    changeEmailScreen: "ChangeEmailScreen",
+    changePasswordScreen: "ChangePasswordScreen"
+}
+
+const global = {
+    root: "GlobalRoot",
+    sendMoneyUser
+}
+
+export const screens = {
+    auth,
+    account,
+    global,
+    tab: {
+        root: "BottomTabRoot",
+        home,
+        payments,
+        wallet
+    }
+}
