93a74520f110ec1c4a4a5adfe3bad34480a8a3d6
Alangobe <alangobe01@gmail.com>
2025-06-08 19:47:59 -0500

    stacks de navegación pt. 1

---
+++ gobepay-front/navigation/stacks/AccountStack.jsx
@@ -0,0 +1,51 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { 
+    AccountScreen, 
+    ChangeEmailScreen, 
+    ChangeFullNameScreen, 
+    ChangePasswordScreen 
+} from "../../screens/Account"
+import { screens } from "../../utils"
+
+const Stack = createNativeStackNavigator()
+
+export default function AccountStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen 
+                name = {screens.account.accountScreen}
+                component={AccountScreen}
+                options={{
+                    title: 'Perfil',
+                    headerTintColor: '#fff',
+                    headerTransparent: true
+                }}
+            />
+            <Stack.Screen 
+                name = {screens.account.changeFullNameScreen}
+                component={ChangeFullNameScreen}
+                options={{
+                    title: 'Editar Nombre',
+                    headerTransparent: true
+                }} 
+            />
+            <Stack.Screen 
+                name = {screens.account.changeEmailScreen}
+                component={ChangeEmailScreen}
+                options={{
+                    title: 'Cambiar email',
+                    headerTransparent: true
+                }} 
+            />
+            <Stack.Screen 
+                name = {screens.account.changePasswordScreen}
+                component={ChangePasswordScreen}
+                options={{
+                    title: 'Cambiar contraseña',
+                    headerTransparent: true
+                }} 
+            />
+
+        </Stack.Navigator>
+    )
+}
